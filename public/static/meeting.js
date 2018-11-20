e05512a58dcb58b93c3fe345512f27aaaa808bf4
Simone Cesano <simonecesano@libero.it>
2018-11-20 06:30:54 +0100

    Added Calendar object

--- public/static/meeting.js
+++ public/static/meeting.js
@@ -3,7 +3,7 @@ var _fix = function(o) {
 	if(typeof o[k] === "object"){
 	    _fix(o[k]);
 	} else {
-	    if (o[k].match(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/i)) {
+	    if (typeof o[k] === "string" && o[k].match(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/i)) {
 		var d = new Date(o[k]);
 		o[k] = d;
 	    } else if (typeof o[k] === "string" && o[k].match(/^false$|^true$/)) {
@@ -17,11 +17,12 @@ var Meeting = function(d) {
     // this = Object.assign(d, object1);;
 
     _fix(d)
+
     d.duration = function(){
 	return this.End - this.Start
     }
 
-    d.IsMine = d.MyResponseType.match(/Organizer/) ? true : false;
+    d.IsMine = d.MyResponseType.match(/Organizer/i) ? true : false;
     d.Start = new Date(d.Start);
     d.End = new Date(d.End);    
     
@@ -55,8 +56,27 @@ var Meeting = function(d) {
 	required: d.RequiredAttendees.Attendee.length,
 	accepted: d.RequiredAttendees.Attendee.filter(e => { return e.ResponseType.match(/Accept/) }).length,
     }
+    d.__proto__ = this.__proto__;
     
     return d;
 }
 
+Meeting.prototype.funnyMethod = function(){
+    console.log("funny method");
+}
 
+
+var Calendar = function(d) {
+    // this = Object.assign(d, object1);;
+
+    // console.log(d.meetings);
+
+    d.meetings
+	.map(e => { _fix(e); return e })
+	.map(e => { e.IsMine = e.IsOrganizer || e.IsFromMe; return e })
+    	.map(e => { e.NeedsWork = !e.MeetingRequestWasSent || e.MyResponseType.match(/NoResponseReceived|Tentative/); return e })
+    
+    d.__proto__ = this.__proto__;
+    
+    return d;
+}
