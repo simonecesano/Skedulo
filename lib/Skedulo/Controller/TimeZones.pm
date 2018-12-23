ddb7056e9aa5a45d788f550f43f8ce5178d4a6a9
Simone Cesano <simonecesano@libero.it>
2018-12-23 17:48:57 +0100

    Refactored, added name conversions, utility unctions, caching

--- lib/Skedulo/Controller/TimeZones.pm
+++ lib/Skedulo/Controller/TimeZones.pm
@@ -3,22 +3,23 @@ package Skedulo::Controller::TimeZones;
 use Mojo::Base 'Mojolicious::Controller';
 use Skedulo::Util qw/pp_xml xml_to_hash/;
 use Data::Dump qw/dump/;
+use Mojo::File qw/path/;
 
 
 my $geo_key = $ENV{GOOGLE_GEO_KEY};
 my $tz_key  = $ENV{GOOGLE_TZ_KEY};
-my $cache = 0;
+my $cache = 1;
 
-sub get_latlon {
+#---------------------------------------------------------------------------------
+
+sub _get_latlon {
     my $c = shift;
 
     my $loc = $c->param('q') || $c->stash('q');
     
     if ($cache && (my $json = $c->chi->get('location::' . $loc))) {
-    	$c->app->log->info(sprintf "location %s cached", $loc);
-    	$c->app->log->info(dump $json->{results}->[0]->{geometry}->{location} );
-    	$c->render(json => $json->{results}->[0]->{geometry}->{location} );
-    	return;
+	my $json = $json->{results}->[0]->{geometry}->{location};
+    	return $json;
     }
     
     my $ua  = Mojo::UserAgent->new();
@@ -26,27 +27,31 @@ sub get_latlon {
 
     $url->query({ address => $loc, key => $geo_key });
     my $tx = $ua->get($url);
-    $c->app->log->info("$url");
-    $c->app->log->info(dump $tx->res->json);
     
     $c->chi->set('location::' . $loc, $tx->res->json);
-    $c->render(json => $tx->res->json->{results}->[0]->{geometry}->{location} );
+    # $c->app->log->info(dump $tx->res->json);
+    my $json = $tx->res->json->{results}->[0]->{geometry}->{location};
+
+    
+    return $json
+
+}
+
+sub get_latlon {
+    my $c = shift;
+    $c->render(json => $c->_get_latlon() );
 };
 
-sub get_timezone {
+#---------------------------------------------------------------------------------
+
+sub _get_timezone {
     my $c = shift;
 
     my $timestamp = $c->param('time') || time();
 
-    # user
-    # location name
-    # lat lon
-
-    if ($cache && (my $json = $c->chi->get((join '::', 'latlng', $c->param('location'))))) {
+    if ($cache && (my $json = $c->chi->get((join '::', 'timezone', $c->param('location'))))) {
     	$c->app->log->info(sprintf "timezone %s cached", $c->param('location'));
-    	$c->app->log->info(dump $json);
-    	$c->render(json => $json );
-    	return;
+    	return $json;
     }
     
     my $ua  = Mojo::UserAgent->new();
@@ -54,11 +59,93 @@ sub get_timezone {
     $url->query({ location => $c->param('location'), timestamp => $timestamp, key => $tz_key });
 
     my $tx = $ua->get($url);
-    $c->app->log->info($tz_key);
-    $c->app->log->info(dump $tx->res->json);
 
-    $c->chi->set((join '::', 'latlng', $c->param('location')), $tx->res->json);
-    $c->render(json => $tx->res->json );
+    my $json = $tx->res->json;
+
+    for (qw/dstOffset rawOffset status/) { delete $json->{$_} }
+    
+    $c->chi->set((join '::', 'timezone', $c->param('location')), $json);
+    return $json;
+};
+
+sub get_timezone {
+    my $c = shift;
+    $c->render(json => $c->_get_timezone() );
+};
+
+#---------------------------------------------------------------------------------
+
+sub set_timezone_for_user {
+    my $c = shift;
+
+    if ($c->param('tz') && $c->cookie('user') || $c->param('who')) {
+	my $json = $c->tz_id_to_name($c->param('tz'));
+	
+	$c->chi->set((join '::', 'timezone', $c->cookie('user') || $c->param('who')), $json);
+	
+	$c->app->log->info($c->param('tz'));
+	$c->app->log->info($c->cookie('user'));
+	$c->app->log->info(dump $c->tz_id_to_name($c->param('tz')));
+
+	$c->render(json => { user => $c->cookie('user') || $c->param('user'), tz => $c->param('tz') });
+    } else {
+	$c->render(json => { error => 'user or timezone missing' });
+    }
+}
+
+sub get_timezone_for_user {
+    my $c = shift;
+
+    if ($cache && (my $json = $c->chi->get((join '::', 'timezone', $c->param('who'))))) {
+    	$c->app->log->info(sprintf "timezone %s cached", $c->param('who'));
+	return $c->render(json => $json );
+    } 
+    
+    $c->stash('name', $c->param('who'));
+    my $tx = $c->get_ews('outlook/whois');
+    my $dom = $tx->res->dom;
+
+    my $city = xml_to_hash($dom->at('City'));
+
+    $c->stash('q', $city);
+
+    my $json = $c->_get_latlon();
+    $c->param('location', join ',', $json->{lat}, $json->{lng}); 
+    $json = $c->_get_timezone();
+
+    $c->chi->set((join '::', 'timezone', $c->param('who')), $json) if $json->{timeZoneId} && $json->{timeZoneName};
+
+    return $c->render(json => $json );
+};
+
+
+sub get_timezone_for_location {
+    my $c = shift;
+
+    $c->stash('q', $c->param('location'));
+
+    my $json = $c->_get_latlon();
+
+    $c->param('location', join ',', $json->{lat}, $json->{lng}); 
+
+    $json = $c->_get_timezone();
+
+    $c->chi->set((join '::', 'timezone', $c->param('location')), $json) if $json->{timeZoneId} && $json->{timeZoneName};
+
+    return $c->render(json => $json );
+}
+
+sub tz_id_to_name {
+    my $c = shift;
+    my $id = shift;
+    
+    state $xml = Mojo::DOM->new(path('windowsZones.xml')->slurp);
+    
+    my $j = xml_to_hash($xml->at(join '', '[type="', $id, '"]'));
+    $j->{timeZoneId} = delete $j->{type};
+    $j->{timeZoneName} = delete $j->{other};
+    delete $j->{territory};
+    return $j;
 };
 
 sub tz_check {
@@ -85,4 +172,65 @@ sub tz_check {
 			       })
 }
 
+
+
 1;
+
+
+my $u = sub {
+    my $c = shift;
+    $c->app->log->info($c->param('tz'));
+    $c->app->log->info($c->cookie('user'));
+    $c->render(json => { user => $c->cookie('user'), tz => $c->param('tz') });
+};
+
+
+use Mojo::File qw/path/;
+
+sub timezones_ews {
+    my $c = shift;
+    my $tx = $c->get_ews('outlook/get_timezones');
+    my $dom = $tx->res->dom;
+
+    path('timezones.xml')->spurt($dom);
+    
+    if ($c->stash('format') eq 'xml' || $c->param('format') eq 'xml') {
+	$c->res->headers->content_type('text/xml');
+	my $id = 'W. Europe Standard Time';
+
+	$dom->find('TimeZoneDefinition');
+	    # ->each(sub{
+	    # 	     my $s = shift;
+	    # 	     $s->remove unless $s->matches(join '', '[Id="', $id, '"]')
+	    # 	 });
+
+	$c->app->log->info($dom->find('TimeZoneDefinition')->size);
+	$c->render(text => $dom )
+    } else {
+	my $json = xml_to_hash($dom->find('TimeZoneDefinition'));
+	$c->render(json => $json )
+    }
+};
+
+1;
+
+__DATA__
+
+use Mojo::File qw/path/;
+
+get '/timezone/:region/*location' => sub {
+    my $c = shift;
+    my $xml = Mojo::DOM->new(path('windowsZones.xml')->slurp);
+
+    my $id = join '/', $c->stash('region'), $c->stash('location');
+    
+    my $j = xml_to_hash($xml->at(join '', '[type="', $id, '"]'));
+    $j->{timeZoneId} = delete $j->{type};
+    $j->{timeZoneName} = delete $j->{other};
+    
+    $c->render( json => $j || 'none' );
+};
+
+1;
+
+__DATA__
