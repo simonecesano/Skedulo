69ffe62ff2f1ee76e5f9f3dd3a7a3886b670b9d6
Simone Cesano <simonecesano@libero.it>
2019-01-02 16:54:09 +0100

    Moved get_freebusy and schedule_meeting from sked.pl

--- lib/Skedulo/Controller/Meetings.pm
+++ lib/Skedulo/Controller/Meetings.pm
@@ -5,6 +5,15 @@ use Skedulo::Util qw/pp_xml xml_to_hash/;
 use Skedulo::DateTime;
 use Data::Dump qw/dump/;
 
+use POSIX qw/floor/;
+
+sub mins_to_hour {
+    my $m = shift;
+    my $h = floor($m / 60);
+    $m = $m % 60;
+    return sprintf '%02d:%02d', $h, $m;
+}
+
 sub dates {
     my $c = shift;
     $c->set_dates()->set_user();
@@ -84,6 +93,81 @@ sub calendar_action {
 
     my $dom = $tx->res->dom;
     return $c->render( text => $dom );
+
+};
+
+sub schedule_meeting {
+    my $c = shift;
+
+    $c->set_user();
+    $c->set_dates();
+    $c->params_to_stash(qw/subject location sensitivity body attendees[] categories[]/);
+
+    my $tx = $c->get_ews('outlook/schedule_meeting');
+    my $dom = $tx->res->dom;
+    
+    for ($c->stash('format')) {
+	/xml/i && do {
+	    return $c->render( text => pp_xml($dom) )
+	};
+	/json/i && do {
+	    my $json;
+	    if ($tx->res->is_success) {
+		# $c->app->log->info($dom);
+		$json = xml_to_hash($dom->at('ResponseCode'));
+		return $c->render( json => { start => $c->stash('start'), end => $c->stash('end'), freebusy => $json } )
+	    } else {
+		$c->res->code(500);
+		$json = { message => $dom->at('ResponseCode')->all_text };
+		return $c->render( json => $json )
+	    }
+	}	    
+    }
+}
+
+sub get_freebusy {
+    my $c = shift;
+
+    $c->set_dates();
+    $c->set_user();
+
+    if ($c->stash('format') =~/(^$)|(html$)/) { return $c->render(template => 'freebusy') }
+
+    $c->stash('interval', 60) unless $c->stash('interval');
+    
+    my $tx = $c->get_ews('outlook/freebusy');
+
+    my $req = $tx->req->body;
+    my $dom = $tx->res->dom;
+
+    for ($c->stash('format')) {
+	/xml/i && do {
+	    return $c->render( text => pp_xml($dom) )
+	};
+	/json/i && do {
+	    my $json;
+	    if ($tx->res->is_success) {
+		my $fb = xml_to_hash($dom->at('MergedFreeBusy'))->{content};
+		my $wp = xml_to_hash($dom->at('WorkingPeriod'));
+
+		$wp->{days} = [ (split /\s+/, delete $wp->{DayOfWeek}) ]; # =~ s/ /, /gr;
+		$wp->{start} = mins_to_hour(delete $wp->{StartTimeInMinutes});
+		$wp->{end} = mins_to_hour(delete $wp->{EndTimeInMinutes});
+
+		return $c->render( json => {
+					    start => $c->stash('start')->set_time_zone('Europe/Berlin'),
+					    interval => $c->stash('interval'),
+					    freebusy => $fb,
+					    working_time => $wp
+					   } )
+	    } else {
+		$c->res->code(500);
+		$c->app->log->debug($req);
+		my $json = { message => eval { $dom->at('ResponseCode')->all_text } || 'N/A', res => $dom, req => $req };
+		return $c->render( json => $json )
+	    }
+	}	    
+    }
 
 };
 
